41a27a2b9061a701edb84913c1bf7c921bdb5e46
Tommi Hämäläinen <tommihmli@gmail.com>
2026-04-06 15:17:01 +0300

    Update jar command to create pomppivasensori.jar

--- package.bat
+++ package.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Java\jdk1.7.0_80\bin\jar" -cfe u.jar ultrasonic.u -C . ultrasonic
+"C:\Program Files\Java\jdk1.7.0_80\bin\jar" -cfe pomppivasensori.jar ultrasonic.pomppivasensori -C . ultrasonic
