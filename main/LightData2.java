0331ca1067f9688248414ef2f87c836945439d98
J-M30 <johannamarjoanttila@gmail.com>
2026-04-28 14:40:13 +0300

    deleting

--- main/LightData2.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public class LightData2 {
-    public float raw;
-    public float filtered;
-    public boolean isBlack;
-}
--- viimeinenYritys/lightData3.java
+++
@@ -1,7 +0,0 @@
-package viimeinenYritys;
-
-public class lightData3 {
-    public float raw;
-    public float filtered;
-    public boolean isBlack;
-}
